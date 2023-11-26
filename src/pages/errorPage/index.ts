24c99f6eb37610305235aecdaa7ce5f18ab49c5d
ZaoBaoo <yurkin.daniil96@gmail.com>
2023-11-26 16:28:16 +0600

    Отредактирован README и добавлена страница 500

--- src/pages/errorPage/index.ts
+++ src/pages/errorPage/index.ts
@@ -4,19 +4,24 @@ import Block from '../../core/Block.ts';
 // Components
 import { Link } from '../../components/link/index.ts';
 
-// Types
-// import { ErrorPageType } from './types.ts';
-
-const def = {
-  code: '404',
-  text: 'Страница не найдена',
-  redirectTo: '/messenger',
-  redirectText: 'Назад к чатам',
+const typeErrorPages: Record<string, unknown> = {
+  '/404': {
+    code: '404',
+    text: 'Страница не найдена',
+    redirectTo: '/messenger',
+    redirectText: 'Назад к чатам',
+  },
+  '/500': {
+    code: '500',
+    text: 'Мы уже фиксим',
+    redirectTo: '/messenger',
+    redirectText: 'Назад к чатам',
+  },
 };
 
 export class ErrorPage extends Block {
   constructor() {
-    super(def);
+    super(typeErrorPages[window.location.pathname]);
   }
 
   init() {
